c4494c5ad9131666e4e06a78e5fa100ab86f0e7e
Павел Селютин <PSelyutin@technoserv.com>
2019-02-07 14:48:58 +0400

    JdbcMealRepositoryImpl and create MealServiceTest

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepositoryImpl.java
@@ -5,7 +5,6 @@ import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
@@ -37,14 +36,7 @@ public class JdbcUserRepositoryImpl implements UserRepository {
 
     @Override
     public User save(User user) {
-//        MapSqlParameterSource map = new MapSqlParameterSource()
-//                .addValue("id", user.getId())
-//                .addValue("name", user.getName())
-//                .addValue("email", user.getEmail())
-//                .addValue("password", user.getPassword())
-//                .addValue("registered", user.getRegistered())
-//                .addValue("enabled", user.isEnabled())
-//                .addValue("caloriesPerDay", user.getCaloriesPerDay());
+
 
         if (user.isNew()) {
             Number newKey = insertUser.executeAndReturnKey(new BeanPropertySqlParameterSource(user));
@@ -70,7 +62,6 @@ public class JdbcUserRepositoryImpl implements UserRepository {
 
     @Override
     public User getByEmail(String email) {
-//        return jdbcTemplate.queryForObject("SELECT * FROM users WHERE email=?", ROW_MAPPER, email);
         List<User> users = jdbcTemplate.query("SELECT * FROM users WHERE email=?", ROW_MAPPER, email);
         return DataAccessUtils.singleResult(users);
     }
