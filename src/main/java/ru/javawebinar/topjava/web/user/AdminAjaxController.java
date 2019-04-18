d76dc368242822847ee87ef7e0f0ece8120545b8
Павел Селютин <PSelyutin@technoserv.com>
2019-04-18 09:43:56 +0400

    first commit

--- src/main/java/ru/javawebinar/topjava/web/user/AdminAjaxController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/AdminAjaxController.java
@@ -13,6 +13,8 @@ import javax.validation.Valid;
 import java.util.List;
 import java.util.StringJoiner;
 
+import static ru.javawebinar.topjava.util.ValidationUtil.generateJoinerErrors;
+
 @RestController
 @RequestMapping("/ajax/admin/users")
 public class AdminAjaxController extends AbstractUserController {
@@ -39,18 +41,7 @@ public class AdminAjaxController extends AbstractUserController {
     @PostMapping
     public ResponseEntity<String> createOrUpdate(@Valid UserTo userTo, BindingResult result) {
         if (result.hasErrors()) {
-            StringJoiner joiner = new StringJoiner("<br>");
-            result.getFieldErrors().forEach(
-                    fe -> {
-                        String msg = fe.getDefaultMessage();
-                        if (msg != null) {
-                            if (!msg.startsWith(fe.getField())) {
-                                msg = fe.getField() + ' ' + msg;
-                            }
-                            joiner.add(msg);
-                        }
-                    });
-            return new ResponseEntity<>(joiner.toString(), HttpStatus.UNPROCESSABLE_ENTITY);
+            return new ResponseEntity<>(generateJoinerErrors(result), HttpStatus.UNPROCESSABLE_ENTITY);
         }
         if (userTo.isNew()) {
             super.create(UserUtil.createNewFromTo(userTo));
